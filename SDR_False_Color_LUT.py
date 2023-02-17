c48d42d4a3a48fc3e43dfc54623af72cc1293700
EaryChow <59176246+EaryChow@users.noreply.github.com>
2023-02-16 19:14:14 -0800

    Don't use 3D LUT, use 3x1D LUT instead

--- SDR_False_Color_LUT.py
+++ SDR_False_Color_LUT.py
@@ -1,8 +1,13 @@
 import colour
 import numpy
+import pathlib
+
+output_config_directory = ""
+output_LUTs_directory = ""
+x_input = numpy.linspace([0.00, 0.00, 0.00], [1.00, 1.00, 1.00], 4096)
 
 # define breaking points, numbers are percentages
-white_lower = 93
+white_lower = 97
 red_lower = 80
 orange_lower = 55
 yellow_lower = 35
@@ -10,78 +15,72 @@ green_yellow_lower = 22
 mid_grey_lower = 16
 green_cyan_lower = 5
 cyan_lower = 0.5
-blue_cyan_lower = 0.02
-blue_lower = 0.005
-black_lower = 0.0005
+blue_cyan_lower = 0.05
+blue_lower = 0.0001
+black_lower = 0
 
 
-def main():
-    # resolution of the 3DLUT
-    LUT_res = 45
+def false_color(col):
+    # col = colour.models.exponent_function_basic(col, 3.0, 'basicFwd')
 
-    LUT = colour.LUT3D(name=f'False Color LUT for AgX Imagery',
-                       size=LUT_res)
+    col = col * 100
 
-    LUT.domain = ([[0.0, 0.0, 0.0], [1.0, 1.0, 1.0]])
-    LUT.comments = [f'A Post Formation False Color LUT for AgX Image Formation',
-                    f'This LUT expects values from 0.0 to 1.0, with inverse power function of 3.0 for precision reason']
+    original = col
 
-    x, y, z, _ = LUT.table.shape
+    # black zone
+    col = colour.algebra.lerp(numpy.greater(original, black_lower), col, [0.0, 0.0, 0.0])
 
-    for i in range(x):
-        for j in range(y):
-            for k in range(z):
-                col = numpy.array(LUT.table[i][j][k], dtype=numpy.longdouble)
+    # blue zone
+    col = colour.algebra.lerp(numpy.greater(original, blue_lower), col, [0.0, 0.0, 1.0])
 
-                col = colour.models.exponent_function_basic(col, 3.0, 'basicFwd')
+    # blue cyan zone
+    col = colour.algebra.lerp(numpy.greater(original, blue_cyan_lower), col, [0.0, 0.5, 1.0])
 
-                col = col * 100
+    # cyan zone
+    col = colour.algebra.lerp(numpy.greater(original, cyan_lower), col, [0.0, 1.0, 1.0])
 
-                original = col
+    # green cyan zone
+    col = colour.algebra.lerp(numpy.greater(original, green_cyan_lower), col, [0.0, 1.0, 0.5])
 
-                # black zone
-                col = colour.algebra.lerp(numpy.greater(original, black_lower), col, [0.0, 0.0, 0.0])
+    # middle grey zone
+    col = colour.algebra.lerp(numpy.greater(original, mid_grey_lower), col, [0.5, 0.5, 0.5])
 
-                # blue zone
-                col = colour.algebra.lerp(numpy.greater(original, blue_lower), col, [0.0, 0.0, 1.0])
+    # green yellow zone
+    col = colour.algebra.lerp(numpy.greater(original, green_yellow_lower), col, [0.5, 1, 0.0])
 
-                # blue cyan zone
-                col = colour.algebra.lerp(numpy.greater(original, blue_cyan_lower), col, [0.0, 0.5, 1.0])
+    # yellow zone
+    col = colour.algebra.lerp(numpy.greater(original, yellow_lower), col, [1.0, 1.0, 0.0])
 
-                # cyan zone
-                col = colour.algebra.lerp(numpy.greater(original, cyan_lower), col, [0.0, 1.0, 1.0])
+    # orange zone
+    col = colour.algebra.lerp(numpy.greater(original, orange_lower), col, [1.0, 0.5, 0.0])
 
-                # green cyan zone
-                col = colour.algebra.lerp(numpy.greater(original, green_cyan_lower), col, [0.0, 1.0, 0.5])
+    # red zone
+    col = colour.algebra.lerp(numpy.greater(original, red_lower), col, [1.0, 0.0, 0.0])
 
-                # middle grey zone
-                col = colour.algebra.lerp(numpy.greater(original, mid_grey_lower), col, [0.5, 0.5, 0.5])
+    # white zone
+    col = colour.algebra.lerp(numpy.greater(original, white_lower), col, [1.0, 1.0, 1.0])
 
-                # green yellow zone
-                col = colour.algebra.lerp(numpy.greater(original, green_yellow_lower), col, [0.5, 1, 0.0])
+    return col
 
-                # yellow zone
-                col = colour.algebra.lerp(numpy.greater(original, yellow_lower), col, [1.0, 1.0, 0.0])
 
-                # orange zone
-                col = colour.algebra.lerp(numpy.greater(original, orange_lower), col, [1.0, 0.5, 0.0])
+y_LUT = false_color(x_input)
 
-                # red zone
-                col = colour.algebra.lerp(numpy.greater(original, red_lower), col, [1.0, 0.0, 0.0])
+LUT_name = "AgX_False_Color"
+LUT_safe = LUT_name.replace(" ", "_")
+LUT = colour.LUT3x1D(
+    table=y_LUT,
+    name="AgX_False_Color"
+)
 
-                # white zone
-                col = colour.algebra.lerp(numpy.greater(original, white_lower), col, [1.0, 1.0, 1.0])
-
-                LUT.table[i][j][k] = numpy.array(col, dtype=LUT.table.dtype)
-
-    colour.write_LUT(
-        LUT,
-        f"AgX_False_Color.cube")
+try:
+    output_directory = pathlib.Path(output_config_directory)
+    LUTs_directory = output_directory / output_LUTs_directory
+    LUT_filename = pathlib.Path(
+        LUTs_directory / "{}.spi1d".format(LUT_safe)
+    )
+    LUTs_directory.mkdir(parents=True, exist_ok=True)
+    colour.io.luts.write_LUT(LUT, LUT_filename, method="Sony SPI1D")
     print(LUT)
+except Exception as ex:
+    raise ex
 
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
